d0147b7df0373bac249bb2deefddfab8f6cf176a
AlexanderArefyev <aleksandrarefev99@gmail.com>
2023-02-24 21:25:49 +0300

    Start App

---
+++ ToDo!/ToDo_App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_App.swift
+//  ToDo!
+//
+//  Created by Александр Арефьев on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
